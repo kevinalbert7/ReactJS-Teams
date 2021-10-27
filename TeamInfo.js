116cc807514d7e863f61723b57de7b26926b2672
kevinalbert7 <20ko@live.fr>
2021-10-27 10:14:15 +0200

    teams

---
+++ TeamInfo.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+class TeamInfo extends React.Component {
+    constructor () {
+        super () 
+
+    }
+    
+    render () {
+        return (
+            <div>
+            </div>
+        )
+    }
+}
+
+export default TeamInfo
